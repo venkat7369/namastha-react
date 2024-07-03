fc68faa9086c534bfe895166f154fc348fb1a447
venkat7369 <114426562+venkat7369@users.noreply.github.com>
2024-07-03 13:50:53 -0400

    Added react all files which are necessary

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement("div",{ id: "parent"}, [
     React.createElement("div", {id: "child" }, [
-        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h1", {}, "this is reaqct cousr"),
         React.createElement("h2", {}, "I am h2 tag")  
     ]),
     React.createElement("div", { id: "child2" }, [
