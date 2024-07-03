25bc78b3f3a03d1c5ebb76c1bd7389ab9cb0db96
venkat7369 <114426562+venkat7369@users.noreply.github.com>
2024-07-03 11:40:47 -0400

    episode 1

---
+++ README.md
@@ -0,0 +1,1 @@
+#namstha react
---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+const parent = React.createElement("div",{ id: "parent"}, [
+    React.createElement("div", {id: "child" }, [
+        React.createElement("h1", {}, "I am h1 tag"),
+        React.createElement("h2", {}, "I am h2 tag")  
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "i am a h1 tag"),
+        React.createElement("h2", {}, "i am h2 tag"),
+    ]),
+]);
+
+
+console.log(parent);  //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+
+
+
+
+
+//parent is an object which is known as elemnet 
+//it becomes the html when it comes to browser
